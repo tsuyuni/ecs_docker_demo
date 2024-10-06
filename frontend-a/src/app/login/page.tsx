bbe2c43d601d9e754803130bbc44653cab0e8a58
tsuyuni <tsuyu.lit@gmail.com>
2024-10-06 16:55:14 +0900

    [add] login page

---
+++ frontend-a/src/app/login/page.tsx
@@ -0,0 +1,12 @@
+const LoginPage = async () => {
+  return (
+    <form>
+      <input />
+      <input />
+      <button>Login</button>
+      <h2>Login</h2>
+    </form>
+  );
+};
+
+export default LoginPage;
