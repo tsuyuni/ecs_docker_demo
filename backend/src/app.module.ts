694abc774661d006899a67ce5e325d528ba2a9f5
tsuyuni <tsuyu.lit@gmail.com>
2024-10-14 22:54:34 +0900

    [add] auth controller

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TodoController } from "./controllers/todo.controller";
+import { AuthController } from "./controllers/auth.controller";
 
 @Module({
   imports: [],
-  controllers: [AppController, TodoController],
+  controllers: [AppController, TodoController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
